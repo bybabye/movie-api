3dc62eed5fbf076389b45e6256cb035d2c179ed9
bybabye <89548252+bybabye@users.noreply.github.com>
2022-12-15 17:09:48 +0700

    update page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 
 const url = "https://2embed.org/library";
 const movie = "https://2embed.org/view/";
-
+const thumbnails = [];
 // SET UP
 const app = express();
 
@@ -28,7 +28,6 @@ app.use(
 //function get
 
 app.get("/v1", (req, resp) => {
-  const thumbnails = [];
   const limit = Number(req.query.limit);
   try {
     axios(url, {
@@ -47,10 +46,14 @@ app.get("/v1", (req, resp) => {
           url,
         });
       });
+       console.log(thumbnails.length);
       if (limit && limit > 0) {
         //
+       
         resp.status(200).json(thumbnails.slice(0, limit));
+        
       } else {
+
         resp.status(200).json(thumbnails);
       }
     });
@@ -58,9 +61,26 @@ app.get("/v1", (req, resp) => {
     resp.status(500).json(err);
   }
 });
+const page = (page,start,end) => {
+  app.get(`/v1/page${page}`, (req, resp) => {
+    try {
+      resp.status(200).json(thumbnails.splice(start,end));
+    } catch (error) {
+      resp.status(500).json(err);
+    }
+  });
+}
 
-
-
+page(1,0,23)
+page(2,23,47)
+page(3,47,71)
+page(4,71,95)
+page(5,71,119)
+page(6,119,143)
+page(7,143,167)
+page(8,167,191)
+page(9,191,215)
+page(10,215,239)
 app.get(`/v1/:character`, (req, resp) => {
   let url = movie + req.params.character;
   const thumbnails = [];
